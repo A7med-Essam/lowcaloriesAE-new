db7dfd175a3f706e6b50d4603c5567c7a165a958
Ahmed Essam <70452432+A7med-Essam@users.noreply.github.com>
2024-03-05 22:15:05 +0200

    add ramadan slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,7 @@ import { Image } from 'primeng/image';
 })
 export class HomeComponent implements OnInit {
   responsiveOptions: any[] ;
-  slider:string[]=['slider-1','slider-2','slider-3'];
+  slider:string[]=['slider-1','slider-3','slider-4','slider-5','slider-6','slider-7','slider-8'];
   constructor() {
     this.responsiveOptions = [
       {
